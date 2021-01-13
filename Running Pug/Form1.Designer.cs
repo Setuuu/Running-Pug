b9199dc42e557106a306307642c2914be5cb857f
Setu Patel <setu.patel2@ontariotechu.net>
2021-01-13 06:19:04 -0500

    Add project files.

---
+++ Running Pug/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Running_Pug
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textLives = new System.Windows.Forms.Label();
+            this.textTime = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.heart = new System.Windows.Forms.PictureBox();
+            this.pug = new System.Windows.Forms.PictureBox();
+            this.fireHydrant = new System.Windows.Forms.PictureBox();
+            this.counter = new System.Windows.Forms.Timer(this.components);
+            this.finalTime = new System.Windows.Forms.Label();
+            this.finalMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pug)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fireHydrant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textLives
+            // 
+            this.textLives.AutoSize = true;
+            this.textLives.Font = new System.Drawing.Font("Century Schoolbook", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLives.Location = new System.Drawing.Point(677, 9);
+            this.textLives.Name = "textLives";
+            this.textLives.Size = new System.Drawing.Size(72, 25);
+            this.textLives.TabIndex = 4;
+            this.textLives.Text = "Lives:";
+            this.textLives.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textTime
+            // 
+            this.textTime.AutoSize = true;
+            this.textTime.Font = new System.Drawing.Font("Century Schoolbook", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTime.Location = new System.Drawing.Point(12, 9);
+            this.textTime.Name = "textTime";
+            this.textTime.Size = new System.Drawing.Size(69, 25);
+            this.textTime.TabIndex = 5;
+            this.textTime.Text = "Time:";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(-10, 402);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(848, 58);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // heart
+            // 
+            this.heart.AccessibleName = "heart";
+            this.heart.Image = global::Running_Pug.Properties.Resources.heartSmaller1;
+            this.heart.Location = new System.Drawing.Point(672, 196);
+            this.heart.Name = "heart";
+            this.heart.Size = new System.Drawing.Size(50, 50);
+            this.heart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.heart.TabIndex = 3;
+            this.heart.TabStop = false;
+            this.heart.Tag = "heart";
+            // 
+            // pug
+            // 
+            this.pug.BackColor = System.Drawing.Color.Transparent;
+            this.pug.Image = global::Running_Pug.Properties.Resources.runningPug_smaller;
+            this.pug.Location = new System.Drawing.Point(12, 331);
+            this.pug.Name = "pug";
+            this.pug.Size = new System.Drawing.Size(114, 75);
+            this.pug.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pug.TabIndex = 2;
+            this.pug.TabStop = false;
+            this.pug.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // fireHydrant
+            // 
+            this.fireHydrant.BackColor = System.Drawing.Color.Transparent;
+            this.fireHydrant.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.fireHydrant.Image = global::Running_Pug.Properties.Resources.fireHydrant_SMALL;
+            this.fireHydrant.Location = new System.Drawing.Point(511, 344);
+            this.fireHydrant.Name = "fireHydrant";
+            this.fireHydrant.Size = new System.Drawing.Size(40, 59);
+            this.fireHydrant.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fireHydrant.TabIndex = 6;
+            this.fireHydrant.TabStop = false;
+            this.fireHydrant.Tag = "fireHydrant";
+            // 
+            // counter
+            // 
+            this.counter.Enabled = true;
+            this.counter.Interval = 1000;
+            this.counter.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // finalTime
+            // 
+            this.finalTime.AutoSize = true;
+            this.finalTime.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalTime.Location = new System.Drawing.Point(303, 196);
+            this.finalTime.Name = "finalTime";
+            this.finalTime.Size = new System.Drawing.Size(0, 30);
+            this.finalTime.TabIndex = 7;
+            this.finalTime.Tag = "finalTime";
+            this.finalTime.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.finalTime.Click += new System.EventHandler(this.finalTime_Click);
+            // 
+            // finalMessage
+            // 
+            this.finalMessage.AutoSize = true;
+            this.finalMessage.Font = new System.Drawing.Font("Century Schoolbook", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalMessage.Location = new System.Drawing.Point(126, 114);
+            this.finalMessage.Name = "finalMessage";
+            this.finalMessage.Size = new System.Drawing.Size(0, 34);
+            this.finalMessage.TabIndex = 8;
+            this.finalMessage.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.finalMessage);
+            this.Controls.Add(this.finalTime);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textTime);
+            this.Controls.Add(this.textLives);
+            this.Controls.Add(this.heart);
+            this.Controls.Add(this.pug);
+            this.Controls.Add(this.fireHydrant);
+            this.Name = "Form1";
+            this.Text = "Running Pug";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.isKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.isKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pug)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fireHydrant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pug;
+        private System.Windows.Forms.PictureBox heart;
+        private System.Windows.Forms.Label textLives;
+        private System.Windows.Forms.Label textTime;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox fireHydrant;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer counter;
+        private System.Windows.Forms.Label finalTime;
+        private System.Windows.Forms.Label finalMessage;
+    }
+}
+
---
+++ Running Pug/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.PerformanceData;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Running_Pug
+{
+    public partial class Form1 : Form
+    {
+
+        bool jump = false;
+        int jumpSpeed = 15;
+        double gravity = 18;
+        int time = 0;
+        int lives = 5;
+        int obstacleSpeed = 10;
+        int heartSpeed = 10;
+        int heartShowRate; 
+
+        Random rand = new Random();
+        bool isGameOver = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            reset();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            pug.Top += jumpSpeed;
+            textLives.Text = "Lives: " + lives;
+
+            if (jump == true && gravity < 0)
+            {
+                jump = false;
+            }
+
+            if (jump == true)
+            {
+                jumpSpeed = -15;
+                gravity -= 1;
+            } else
+            {
+                jumpSpeed = 15;
+            }
+
+            if (pug.Top > 183 && jump == false)
+            {
+                gravity = 18;
+                pug.Top = 186;
+                jumpSpeed = 0;
+            }
+            fireHydrantMovement();
+            heartMovement();
+
+            checkDeath();
+        }
+
+        //Function that controls the movement of the firehydrant
+        private void fireHydrantMovement()
+        {
+            obstacleSpeed = rand.Next(1, 10);
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "fireHydrant")
+                {
+
+                    x.Left -= obstacleSpeed;
+                    if (x.Left < -100)
+                    {
+                       x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                    }
+
+                    if(pug.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        fireHydrant.Location = new Point(900, 205);
+                        lives-= 1;
+                    }
+                }
+            }
+        }
+
+        //Function that controls the movement of the heart
+        private void heartMovement()
+        {
+            heartSpeed = rand.Next(5, 10);
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "heart")
+                {
+                    //if statement to randomize the appearance of the heart
+                    heartShowRate = rand.Next(1, 20);
+                    if (heartShowRate % 4 == 0)
+                    {
+                        x.Left -= heartSpeed;
+                        if (x.Left < -100)
+                        {
+                            x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        }
+
+                    }
+
+                    if (pug.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        heart.Location = new Point(900, 135);
+                        lives += 1;
+                    }
+                }
+            }
+        }
+
+        private void isKeyDown(object sender, KeyEventArgs e)
+        {
+            //checks if the player is pressing the UP key, and if they are already jumping 
+            if(e.KeyCode == Keys.Up && jump == false)
+            {
+                jump = true;
+            }
+        }
+
+        private void isKeyUp(object sender, KeyEventArgs e)
+        {
+            if (jump == true)
+            {
+                jump = false;
+            }
+
+            //Check if the player wants to reset the game state
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                reset();
+            }
+        }
+
+        //Function to reset the game back to its orignal state, ready to start again
+        private void reset()
+        {
+            gravity = 18;
+            jumpSpeed = 0;
+            jump = false;
+            time = 0;
+            obstacleSpeed = 10;
+
+            textTime.Text = "Time: " + time;
+            lives = 5;
+            textLives.Text ="Lives: " + lives;
+
+            pug.Image = Properties.Resources.runningPug_smaller;
+            isGameOver = false;
+            pug.Top = 186;
+
+            fireHydrant.Top = 205;
+            heart.Top = 135;
+
+            gameTimer.Start();
+        }
+
+
+        //Timer counter with 1000 interval to tick each second
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            textTime.Text = "Time: " + time;
+
+        }
+
+        private void checkDeath()
+        {
+            if (lives < 0)
+            {
+                isGameOver = true;
+                gameTimer.Stop();
+                counter.Stop();
+                finalTime.Text = "Final Time: " + time;
+                finalMessage.Text = "Press 'R' to beat that lame score of yours!";
+            }
+        }
+
+        private void finalTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
